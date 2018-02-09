ccf76c9d9c07864b0cc903d854a35f9658b8bf11
Kuba Mikita <jakub@underdev.it>
2018-02-09 08:09:23 +0100

    admin field description

--- class/Defaults/Recipient/Administrator.php
+++ class/Defaults/Recipient/Administrator.php
@@ -46,6 +46,7 @@ class Administrator extends Abstracts\Recipient {
 			'css_class'   => 'recipient-value', // don't edit this!
 			'value'       => $this->get_default_value(),
 			'placeholder' => $this->get_default_value(),
+			'description' => sprintf( __( 'You can edit this email in <a href="%s">General Settings</a>' ), admin_url( 'options-general.php' ) ),
 			'disabled'    => true,
 		) );
 
