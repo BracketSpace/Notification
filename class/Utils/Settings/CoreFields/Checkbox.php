65bb34202c2e04b3c27491b464ca8f3ffedec0cc
Kuba Mikita <jakub@underdev.it>
2018-02-10 17:53:07 +0100

    settings source files

---
+++ class/Utils/Settings/CoreFields/Checkbox.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Checkbox field class
+ *
+ * @package notification
+ */
+
+namespace underDEV\Notification\Utils\Settings\CoreFields;
+
+/**
+ * Checkbox class
+ */
+class Checkbox {
+
+	/**
+	 * Checkbox field
+	 * Requires 'label' addon
+     *
+	 * @param  Field $field Field instance.
+	 * @return void
+	 */
+	public function input( $field ) {
+
+		echo '<label><input type="checkbox" id="' . $field->input_id() . '" name="' . $field->input_name() . '" value="true" ' . checked( $field->value(), 'true', false ) . '> ' . $field->addon( 'label' ) . '</label>';
+
+	}
+
+	/**
+	 * Sanitize checkbox value
+	 * Allows only for empty string and 'true'
+     *
+	 * @param  string $value saved value.
+	 * @return string        empty string or 'true'
+	 */
+	public function sanitize( $value ) {
+
+		return ( $value !== 'true' ) ? '' : $value;
+
+	}
+
+}
---
+++ class/Utils/Settings/CoreFields/Url.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Url field class
+ *
+ * @package notification
+ */
+
+namespace underDEV\Notification\Utils\Settings\CoreFields;
+
+/**
+ * Url class
+ */
+class Url {
+
+	/**
+	 * Url field
+     *
+	 * @param  Field $field Field instance.
+	 * @return void
+	 */
+	public function input( $field ) {
+
+		echo '<label><input type="url" id="' . $field->input_id() . '" name="' . $field->input_name() . '" value="' . $field->value() . '" class="widefat"></label>';
+
+	}
+
+	/**
+	 * Sanitize input value
+     *
+	 * @param  string $value saved value.
+	 * @return string        sanitized url
+	 */
+	public function sanitize( $value ) {
+
+		return esc_url_raw( $value );
+
+	}
+
+}
