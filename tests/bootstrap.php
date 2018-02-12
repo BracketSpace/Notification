6ffbdd39e80976f84b3bfe7fc2ea2878e9625a5a
Kuba Mikita <jakub@underdev.it>
2018-02-12 15:58:07 +0100

    paths adjustment

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -23,7 +23,7 @@ require_once $_tests_dir . '/includes/functions.php';
  * Manually load the plugin being tested.
  */
 function _manually_load_plugin() {
-	require dirname( dirname( __FILE__ ) ) . '/sample-plugin.php';
+	require dirname( dirname( __FILE__ ) ) . '/notification.php';
 }
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
--- tests/test-sample.php
+++ tests/test-sample.php
@@ -2,7 +2,7 @@
 /**
  * Class SampleTest
  *
- * @package Sample_Plugin
+ * @package notification
  */
 
 /**
@@ -13,7 +13,7 @@ class SampleTest extends WP_UnitTestCase {
 	/**
 	 * A single example test.
 	 */
-	function test_sample() {
+	public function test_sample() {
 		// Replace this with some actual testing code.
 		$this->assertTrue( true );
 	}
