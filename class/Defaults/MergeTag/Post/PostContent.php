19e4bc6a7f614ed8b4b7e6c07c54ceebb4b155a7
Kuba Mikita <jakub@underdev.it>
2018-02-17 16:49:31 +0100

    post content

---
+++ class/Defaults/MergeTag/Post/PostContent.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * Post content merge tag
+ *
+ * Requirements:
+ * - Trigger property of the post type slug with WP_Post object
+ *
+ * @package notification
+ */
+
+namespace underDEV\Notification\Defaults\MergeTag\Post;
+
+use underDEV\Notification\Defaults\MergeTag\StringTag;
+
+
+/**
+ * Post content merge tag class
+ */
+class PostContent extends StringTag {
+
+	/**
+	 * Post Type slug
+	 *
+	 * @var string
+	 */
+	protected $post_type;
+
+	/**
+     * Merge tag constructor
+     *
+     * @since [Next]
+     * @param array $params merge tag configuration params.
+     */
+    public function __construct( $params = array() ) {
+
+    	if ( isset( $params['post_type'] ) ) {
+    		$this->post_type = $params['post_type'];
+    	} else {
+    		$this->post_type = 'post';
+    	}
+
+    	$args = wp_parse_args( $params, array(
+			'slug'        => $this->post_type . '_content',
+			// translators: singular post name.
+			'name'        => sprintf( __( '%s content' ), $this->get_nicename() ),
+			'description' => __( 'Welcome to WordPress. This is your first post. Edit or delete it, then start writing!' ),
+			'example'     => true,
+			'resolver'    => function() {
+				return apply_filters( 'the_content', $this->trigger->{ $this->post_type }->post_content );
+			},
+		) );
+
+    	parent::__construct( $args );
+
+	}
+
+	/**
+	 * Function for checking requirements
+	 *
+	 * @return boolean
+	 */
+	public function check_requirements() {
+		return isset( $this->trigger->{ $this->post_type }->post_content );
+	}
+
+	/**
+	 * Gets nice, translated post name
+	 *
+	 * @since  [Next]
+	 * @return string post name
+	 */
+	public function get_nicename() {
+		return get_post_type_object( $this->post_type )->labels->singular_name;
+	}
+
+}
