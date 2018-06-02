18c0448a0a5570e26610ca0b3cc192f5e69e63d2
Kuba Mikita <jakub@underdev.it>
2018-06-02 20:36:11 +0200

    namespace fix

--- tests/core/test-main.php
+++ tests/core/test-main.php
@@ -26,7 +26,7 @@ class MainTest extends WP_UnitTestCase {
 	 * @since [Next]
 	 */
 	public function test_runtime() {
-		$this->assertInstanceOf( 'underDEV\Notification\Runtime', $this->notification );
+		$this->assertInstanceOf( 'BracketSpace\Notification\Runtime', $this->notification );
 	}
 
 	/**
@@ -37,22 +37,22 @@ class MainTest extends WP_UnitTestCase {
 	public function test_boot() {
 
 		// Instances.
-		$this->assertInstanceOf( 'underDEV\Notification\Utils\View', $this->notification->view() );
-		$this->assertInstanceOf( 'underDEV\Notification\Utils\Ajax', $this->notification->ajax() );
-		$this->assertInstanceOf( 'underDEV\Notification\Admin\BoxRenderer', $this->notification->boxrenderer() );
-		$this->assertInstanceOf( 'underDEV\Notification\Admin\FormRenderer', $this->notification->formrenderer() );
-		$this->assertInstanceOf( 'underDEV\Notification\Utils\Files', $this->notification->files );
-		$this->assertInstanceOf( 'underDEV\Notification\Internationalization', $this->notification->internationalization );
-		$this->assertInstanceOf( 'underDEV\Notification\Admin\Settings', $this->notification->settings );
-		$this->assertInstanceOf( 'underDEV\Notification\Admin\PostData', $this->notification->post_data );
-		$this->assertInstanceOf( 'underDEV\Notification\Admin\Trigger', $this->notification->admin_trigger );
-		$this->assertInstanceOf( 'underDEV\Notification\Admin\Notifications', $this->notification->admin_notifications );
-		$this->assertInstanceOf( 'underDEV\Notification\Admin\PostType', $this->notification->admin_post_type );
-		$this->assertInstanceOf( 'underDEV\Notification\Admin\PostTable', $this->notification->admin_post_table );
-		$this->assertInstanceOf( 'underDEV\Notification\Admin\MergeTags', $this->notification->admin_merge_tags );
-		$this->assertInstanceOf( 'underDEV\Notification\Admin\Scripts', $this->notification->admin_scripts );
-		$this->assertInstanceOf( 'underDEV\Notification\Admin\Recipients', $this->notification->admin_recipients );
-		$this->assertInstanceOf( 'underDEV\Notification\Admin\Extensions', $this->notification->admin_extensions );
+		$this->assertInstanceOf( 'BracketSpace\Notification\Utils\View', $this->notification->view() );
+		$this->assertInstanceOf( 'BracketSpace\Notification\Utils\Ajax', $this->notification->ajax() );
+		$this->assertInstanceOf( 'BracketSpace\Notification\Admin\BoxRenderer', $this->notification->boxrenderer() );
+		$this->assertInstanceOf( 'BracketSpace\Notification\Admin\FormRenderer', $this->notification->formrenderer() );
+		$this->assertInstanceOf( 'BracketSpace\Notification\Utils\Files', $this->notification->files );
+		$this->assertInstanceOf( 'BracketSpace\Notification\Internationalization', $this->notification->internationalization );
+		$this->assertInstanceOf( 'BracketSpace\Notification\Admin\Settings', $this->notification->settings );
+		$this->assertInstanceOf( 'BracketSpace\Notification\Admin\PostData', $this->notification->post_data );
+		$this->assertInstanceOf( 'BracketSpace\Notification\Admin\Trigger', $this->notification->admin_trigger );
+		$this->assertInstanceOf( 'BracketSpace\Notification\Admin\Notifications', $this->notification->admin_notifications );
+		$this->assertInstanceOf( 'BracketSpace\Notification\Admin\PostType', $this->notification->admin_post_type );
+		$this->assertInstanceOf( 'BracketSpace\Notification\Admin\PostTable', $this->notification->admin_post_table );
+		$this->assertInstanceOf( 'BracketSpace\Notification\Admin\MergeTags', $this->notification->admin_merge_tags );
+		$this->assertInstanceOf( 'BracketSpace\Notification\Admin\Scripts', $this->notification->admin_scripts );
+		$this->assertInstanceOf( 'BracketSpace\Notification\Admin\Recipients', $this->notification->admin_recipients );
+		$this->assertInstanceOf( 'BracketSpace\Notification\Admin\Extensions', $this->notification->admin_extensions );
 
 	}
 
