4f2ea08c6295e214481f3181e62e5c600fe8e574
Sebastian <sebamanczak@gmail.com>
2018-09-08 17:37:32 +0200

    'success'

--- class/Defaults/Trigger/WordPress/ActivePlugin.php
+++ class/Defaults/Trigger/WordPress/ActivePlugin.php
@@ -1,5 +1,11 @@
 <?php
 
+/**
+ * WordPress Active Plugin trigger
+ *
+ * @package notification
+ */
+
 namespace BracketSpace\Notification\Defaults\Trigger\WordPress;
 
 use BracketSpace\Notification\Defaults\MergeTag;
@@ -7,8 +13,9 @@ use BracketSpace\Notification\Abstracts;
 
 class ActivePlugin extends Abstracts\Trigger{
 
-	private $plugin;
-
+	/**
+	 * Constructor
+	 */
 	public function __construct(){
 
 		parent::__construct( 'wordpress/active_plugin',  __( 'Plugin Active', 'notification' ) );
@@ -16,25 +23,78 @@ class ActivePlugin extends Abstracts\Trigger{
 		$this->add_action( 'activated_plugin', 10, 2 );
 		$this->set_group( __( 'WordPress', 'notification' ) );
 		$this->set_description( __( 'Fires when plugin can be active', 'notification' ) );
+
 	}
 
+	/**
+	 * Gets specific update type
+	 *
+	 * @since  5.1.5
+	 * @param  string $plugin
+	 * @return string
+	 */
 	public function action($plugin, $network_activation)
 	{
 		$this->plugin = $plugin;
 	}
 
+
+	/**
+	 * Registers attached merge tags
+	 *
+	 * @return void
+	 */
 	public function merge_tags()
 	{
-		$this->add_merge_tag( new MergeTag\HtmlTag( array(
+		$this->add_merge_tag( new MergeTag\StringTag( array(
 			'slug'        => 'plugin_name',
 			'name'        => __( 'Plugin Name', 'notification' ),
-			'description' => __( 'Name activated plugin.', 'notification' ),
+			'description' => __( 'Name deactivated plugin.', 'notification' ),
 			'resolver'    => function( $trigger ) {
 
-				$plugin_name = explode('/',$this->plugin);
-				$html  =  $plugin_name[0];
+				$plugin_dir = ABSPATH . 'wp-content'.DIRECTORY_SEPARATOR.'plugins'.DIRECTORY_SEPARATOR.$trigger->plugin;
+				$plugin_data = get_plugin_data($plugin_dir);
+				return $plugin_data['Name'];
+			},
+		) ) );
 
-				return $html;
+		$this->add_merge_tag( new MergeTag\StringTag( array(
+			'slug'        => 'plugin_author_name',
+			'name'        => __( 'Author Name', 'notification' ),
+			'description' => __( 'Author of the plugin.', 'notification' ),
+			'resolver'    => function( $trigger ) {
+
+				$plugin_dir = ABSPATH . 'wp-content'.DIRECTORY_SEPARATOR.'plugins'.DIRECTORY_SEPARATOR.$trigger->plugin;
+				$plugin_data = get_plugin_data($plugin_dir);
+
+				$this->description = $plugin_data['Description'];
+				return $plugin_data['AuthorName'];
+			},
+		) ) );
+
+		$this->add_merge_tag( new MergeTag\StringTag( array(
+			'slug'        => 'plugin_version',
+			'name'        => __( 'Plugin Version', 'notification' ),
+			'description' => __( 'Version deactivated plugin.', 'notification' ),
+			'resolver'    => function( $trigger ) {
+
+				$plugin_dir = ABSPATH . 'wp-content'.DIRECTORY_SEPARATOR.'plugins'.DIRECTORY_SEPARATOR.$trigger->plugin;
+				$plugin_data = get_plugin_data($plugin_dir);
+
+				return $plugin_data['Version'];
+			},
+		) ) );
+
+		$this->add_merge_tag( new MergeTag\StringTag( array(
+			'slug'        => 'plugin_url',
+			'name'        => __( 'Plugin adress url', 'notification' ),
+			'description' => __( 'Adress url to plugin.', 'notification' ),
+			'resolver'    => function( $trigger ) {
+
+				$plugin_dir = ABSPATH . 'wp-content'.DIRECTORY_SEPARATOR.'plugins'.DIRECTORY_SEPARATOR.$trigger->plugin;
+				$plugin_data = get_plugin_data($plugin_dir);
+
+				return $plugin_data['PluginURI'];
 			},
 		) ) );
 
--- class/Defaults/Trigger/WordPress/DeactivatedPlugin.php
+++ class/Defaults/Trigger/WordPress/DeactivatedPlugin.php
@@ -11,12 +11,14 @@ namespace BracketSpace\Notification\Defaults\Trigger\WordPress;
 use BracketSpace\Notification\Defaults\MergeTag;
 use BracketSpace\Notification\Abstracts;
 
-
 /**
  * WordPressDeactivate Plugin trigger class
  */
 class DeactivatedPlugin extends Abstracts\Trigger{
 
+	/**
+	 * Constructor
+	 */
 	public function __construct(){
 
 		parent::__construct( 'wordpress/deactived_plugin',  __( 'Plugin Deactivated', 'notification' ) );
@@ -26,6 +28,13 @@ class DeactivatedPlugin extends Abstracts\Trigger{
 		$this->set_description( __( 'Fires when plugin is deactivated', 'notification' ) );
 	}
 
+	/**
+	 * Gets specific update type
+	 *
+	 * @since  5.1.5
+	 * @param  string $plugin
+	 * @return string
+	 */
 	public function action($plugin, $network_activation)
 	{
 		$this->plugin = $plugin;
@@ -33,6 +42,11 @@ class DeactivatedPlugin extends Abstracts\Trigger{
 		$this->plugin_network = $network_activation;
 	}
 
+	/**
+	 * Registers attached merge tags
+	 *
+	 * @return void
+	 */
 	public function merge_tags()
 	{
 		$this->add_merge_tag( new MergeTag\StringTag( array(
@@ -74,6 +88,7 @@ class DeactivatedPlugin extends Abstracts\Trigger{
 			},
 		) ) );
 
+
 		$this->add_merge_tag( new MergeTag\StringTag( array(
 			'slug'        => 'plugin_url',
 			'name'        => __( 'Plugin adress url', 'notification' ),
@@ -84,10 +99,9 @@ class DeactivatedPlugin extends Abstracts\Trigger{
 				$plugin_data = get_plugin_data($plugin_dir);
 
 				return $plugin_data['PluginURI'];
+
 			},
 		) ) );
 
 	}
-
-
 }
