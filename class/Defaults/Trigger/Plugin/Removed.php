14e1064b8504f375dd9ec8257e2eed29efff952b
Sebastian <sebamanczak@gmail.com>
2018-09-13 21:00:27 +0200

    Add removed trigger

---
+++ class/Defaults/Trigger/Plugin/Removed.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * WordPress plugin removed trigger
+ *
+ * @package notification
+ */
+
+namespace BracketSpace\Notification\Defaults\Trigger\Plugin;
+
+use BracketSpace\Notification\Defaults\MergeTag;
+use BracketSpace\Notification\Abstracts;
+
+/**
+ * Removed plugin trigger class
+ */
+class Removed extends PluginTrigger {
+
+	/**
+	 * Constructor
+	 */
+	public function __construct( ) {
+
+		parent::__construct( 'wordpress/plugin/removed', __( 'Plugin removed', 'notification' ) );
+
+		$this->add_action( 'removed_plugin', 10, 2 );
+		$this->set_group( __( 'Plugin', 'notification' ) );
+		$this->set_description( __( 'Fires when plugin is removed', 'notification' ) );
+
+	}
+
+	/**
+	 * Trigger action
+	 *
+	 * @param  string $plugin_rel_path Plugin path.
+	 * @return void
+	 */
+	public function action( $plugin_rel_path ) {
+		$plugin_dir                    = WP_PLUGIN_DIR . DIRECTORY_SEPARATOR . $plugin_rel_path;
+		$this->plugin                  = get_plugin_data( $plugin_dir );
+		$this->plugin_remove_date_time = strtotime( 'now' );
+	}
+
+	/**
+	 * Registers attached merge tags
+	 *
+	 * @return void
+	 */
+	public function merge_tags( )
+	{
+
+		parent::merge_tags( );
+
+		$this->add_merge_tag( new MergeTag\DateTime\DateTime( array(
+			'slug' => 'plugin_remove_date_time',
+			'name' => __( 'Plugin removed date and time', 'notification' ),
+		) ) );
+	}
+}
