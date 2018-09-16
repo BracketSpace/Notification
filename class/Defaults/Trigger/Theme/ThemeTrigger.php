015700a9ba4e308b618a9d4871e2259e4b502c48
Sebastian <sebamanczak@gmail.com>
2018-09-16 14:13:20 +0200

    Add theme merge tags

--- class/Defaults/Trigger/Theme/ThemeTrigger.php
+++ class/Defaults/Trigger/Theme/ThemeTrigger.php
@@ -23,59 +23,27 @@ abstract class ThemeTrigger extends Abstracts\Trigger {
 	public function merge_tags() {
 
 		$this->add_merge_tag(
-			new MergeTag\StringTag(
-				array(
-					'slug'        => 'theme_name',
-					'name'        => __( 'Theme name', 'notification' ),
-					'description' => __( 'Twenty Seventeen', 'notification' ),
-					'example'     => true,
-					'resolver'    => function( $trigger ) {
-						return $trigger->theme->get( 'Name' );
-					},
-				)
-			)
+			new MergeTag\Theme\ThemeAuthor()
 		);
 
 		$this->add_merge_tag(
-			new MergeTag\StringTag(
-				array(
-					'slug'        => 'theme_author_name',
-					'name'        => __( 'Theme author name', 'notification' ),
-					'description' => __( 'the WordPress team', 'notification' ),
-					'example'     => true,
-					'resolver'    => function( $trigger ) {
-						return $trigger->theme->get( 'AuthorName' );
-					},
-				)
-			)
+			new MergeTag\Theme\ThemeAuthorURI()
 		);
 
 		$this->add_merge_tag(
-			new MergeTag\StringTag(
-				array(
-					'slug'        => 'theme_version',
-					'name'        => __( 'Theme version', 'notification' ),
-					'description' => __( '1.0.2', 'notification' ),
-					'example'     => true,
-					'resolver'    => function( $trigger ) {
-						return $trigger->theme->get( 'Version' );
-					},
-				)
-			)
+			new MergeTag\Theme\ThemeDescription()
 		);
 
 		$this->add_merge_tag(
-			new MergeTag\StringTag(
-				array(
-					'slug'        => 'theme_url',
-					'name'        => __( 'Theme adress URL', 'notification' ),
-					'description' => __( 'https://example.com', 'notification' ),
-					'example'     => true,
-					'resolver'    => function( $trigger ) {
-						return $trigger->theme->get( 'ThemeURI' );
-					},
-				)
-			)
+			new MergeTag\Theme\ThemeName()
+		);
+
+		$this->add_merge_tag(
+			new MergeTag\Theme\ThemeURI()
+		);
+
+		$this->add_merge_tag(
+			new MergeTag\Theme\ThemeVersion()
 		);
 
 	}
