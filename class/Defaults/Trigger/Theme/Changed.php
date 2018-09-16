87e4f6dc680876c85629e4c59c8bb6b31bf3fe60
Sebastian <sebamanczak@gmail.com>
2018-09-16 14:17:49 +0200

    Add changed theme trigger

--- class/Defaults/Trigger/Theme/Changed.php
+++ class/Defaults/Trigger/Theme/Changed.php
@@ -8,25 +8,19 @@
 namespace BracketSpace\Notification\Defaults\Trigger\Theme;
 
 use BracketSpace\Notification\Defaults\MergeTag;
-
 /**
  * Changed theme trigger class.
  */
 class Changed extends ThemeTrigger {
-
 	/**
 	 * Constructor.
 	 */
 	public function __construct() {
-
 		parent::__construct( 'wordpress/theme/changed', __( 'Theme changed', 'notification' ) );
-
 		$this->add_action( 'switch_theme', 10, 3 );
 		$this->set_group( __( 'Theme', 'notification' ) );
 		$this->set_description( __( 'Fires when theme is changed', 'notification' ) );
-
 	}
-
 	/**
 	 * Trigger action.
 	 *
@@ -36,25 +30,73 @@ class Changed extends ThemeTrigger {
 	 * @return mixed void or false if no notifications should be sent.
 	 */
 	public function action( $new_name, $new_theme, $old_theme ) {
-
 		$this->theme_changed_date_time = strtotime( 'now' );
 		$this->theme_new              = $new_theme;
 		$this->theme_old              = $old_theme;
-
 	}
-
 	/**
 	 * Registers attached merge tags.
 	 *
 	 * @return void.
 	 */
 	public function merge_tags() {
-
 		parent::merge_tags();
 
-		$this->add_merge_tag( new MergeTag\DateTime\DateTime( array(
-			'slug' => 'theme_changed_date_time',
-			'name' => __( 'Theme changed date and time', 'notification' ),
-		) ) );
+
+		$this->add_merge_tag(
+			new MergeTag\Theme\ThemeAuthor(
+				array(
+					'name' => 'Old theme author name',
+					'description' => 'Twenty fifteen',
+					'type' => 'old'
+				)
+			)
+		);
+
+		$this->add_merge_tag(
+			new MergeTag\Theme\ThemeAuthorURI(
+				array(
+					'name' => 'Old theme author URL',
+					'type' => 'old'
+				)
+			)
+		);
+
+		$this->add_merge_tag(
+			new MergeTag\Theme\ThemeDescription(
+				array(
+					'name' => 'Old theme description',
+					'type' => 'old'
+				)
+			)
+		);
+
+		$this->add_merge_tag(
+			new MergeTag\Theme\ThemeName(
+				array(
+					'name' => 'Old theme name',
+					'type' => 'old'
+				)
+			)
+		);
+
+		$this->add_merge_tag(
+			new MergeTag\Theme\ThemeURI(
+				array(
+					'name' => 'Old theme ',
+					'type' => 'old'
+				)
+			)
+		);
+
+		$this->add_merge_tag(
+			new MergeTag\Theme\ThemeVersion(
+				array(
+					'name' => 'Old theme version',
+					'type' => 'old'
+				)
+			)
+		);
+
 	}
 }
