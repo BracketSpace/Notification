d783d1bd4ed4eaffb71f5a2f8a13bf77aa5ccb21
Łukasz <kontakt@lukaszkajdan.pl>
2018-10-23 08:34:29 +0200

    change merge tags names and changelogs to changelog

--- inc/defaults/global_merge_tags.php
+++ inc/defaults/global_merge_tags.php
@@ -139,7 +139,7 @@ notification_add_global_merge_tag(
 	new MergeTag\DateTime\Date(
 		array(
 			'slug'      => 'date',
-			'name'      => __( 'Current date', 'notification' ),
+			'name'      => __( 'Trigger execution date', 'notification' ),
 			'hidden'    => true,
 			'timestamp' => time(),
 		)
@@ -150,7 +150,7 @@ notification_add_global_merge_tag(
 	new MergeTag\DateTime\DateTime(
 		array(
 			'slug'      => 'date_time',
-			'name'      => __( 'Current date and time', 'notification' ),
+			'name'      => __( 'Trigger execution date and time', 'notification' ),
 			'hidden'    => true,
 			'timestamp' => time(),
 		)
@@ -161,7 +161,7 @@ notification_add_global_merge_tag(
 	new MergeTag\DateTime\Time(
 		array(
 			'slug'      => 'time',
-			'name'      => __( 'Current time', 'notification' ),
+			'name'      => __( 'Trigger execution time', 'notification' ),
 			'hidden'    => true,
 			'timestamp' => time(),
 		)
