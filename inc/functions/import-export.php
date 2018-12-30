9e0b1e36e95a78be14713b597d773f03b3bb5184
Kuba Mikita <jakub@underdev.it>
2018-12-30 18:46:57 +0100

    sync mode

--- inc/functions/import-export.php
+++ inc/functions/import-export.php
@@ -40,3 +40,42 @@ function notification_import_extra( $key, callable $importer ) {
 		call_user_func( $importer, $data, $notification );
 	}, 10, 2 );
 }
+
+/**
+ * Enables the notification syncing
+ * By default path used is current theme's `notifiations` dir.
+ *
+ * @since  [Next]
+ * @param  mixed $path full json directory path or null to use default.
+ * @return void
+ */
+function notification_sync( $path = null ) {
+
+	if ( ! $path ) {
+		$path = trailingslashit( get_stylesheet_directory() ) . 'notifications';
+	}
+
+	if ( ! file_exists( $path ) ) {
+		mkdir( $path );
+	}
+
+	if ( ! file_exists( trailingslashit( $path ) . 'index.php' ) ) {
+		file_put_contents( trailingslashit( $path ) . 'index.php', '<?php' . "\r\n" . '// Keep this file here.' . "\r\n" ); // phpcs:ignore
+	}
+
+	add_filter( 'notification/sync/dir', function( $dir ) use ( $path ) {
+		return $path;
+	} );
+
+}
+
+/**
+ * Checks if the plugin is in syncing mode
+ *
+ * @since [Next]
+ * @return boolean
+ */
+function notification_is_syncing() {
+	return (bool) apply_filters( 'notification/sync/dir', false );
+}
+
