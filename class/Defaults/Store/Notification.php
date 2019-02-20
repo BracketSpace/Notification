536107fc1d575f79d59c84f4754466189fa6380c
Kuba Mikita <jakub@underdev.it>
2019-02-20 09:12:41 +0100

    adding notifications from wp posts

--- class/Defaults/Store/Notification.php
+++ class/Defaults/Store/Notification.php
@@ -20,6 +20,44 @@ class Notification extends Abstracts\Store {
 	const STORAGE_KEY = 'notification';
 
 	/**
+	 * {@inheritdoc}
+	 *
+	 * Compares the version of Notification object, and replace it if adding the newer.
+	 *
+	 * @since  [Next]
+	 * @throws \Exception If no offset has been provided and item doesn't implements Nameable.
+	 * @param  mixed $offset Offset.
+	 * @param  mixed $value  Value.
+	 * @return void
+	 */
+	public function offsetSet( $offset, $value ) {
+
+		// Prepare offset.
+		if ( empty( $offset ) ) {
+			if ( ! ( $value instanceof Interfaces\Nameable ) ) {
+				throw new \Exception( 'You must provide the item key or use "Nameable" object' );
+			} else {
+				$offset = $value->get_slug();
+			}
+		}
+
+		// Add to Store.
+		add_filter( self::get_storage_key(), function( $items ) use ( $offset, $value ) {
+
+			// Check if newer version hasn't been added already.
+			if ( isset( $items[ $offset ] ) && version_compare( $items[ $offset ]->get_version(), $value->get_version(), '>=' ) ) {
+				return $items;
+			}
+
+			$items[ $offset ] = $value;
+
+			return $items;
+
+		} );
+
+	}
+
+	/**
 	 * Gets the Notifications with specific Trigger
 	 *
 	 * @since  [Next]
