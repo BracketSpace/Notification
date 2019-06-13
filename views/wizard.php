1ce7b5c6543e70a3be2a642543a38571e89eb01c
Jakub Mikita <jakub@bracketspace.com>
2019-06-13 12:11:26 +0200

    wizard frontend adjustments

--- views/wizard.php
+++ views/wizard.php
@@ -10,149 +10,36 @@
 <div id="notifications-wizard" data-selected-notifications-count="0">
 
 	<div class="content">
-		<h1>Notification Wizard</h1>
-		<h3>Quickly setup the Notifications you need.<br/>
-		You'll be able to edit them later.</h3>
+		<h1><?php esc_html_e( 'Notification Wizard', 'notification' ); ?></h1>
+		<h3><?php esc_html_e( 'Quickly setup the Notifications you need.', 'notification' ); ?><br/>
+		<?php esc_html_e( 'You\'ll be able to edit them later.', 'notification' ); ?></h3>
 
 		<div class="notifications-group">
 			<h2 class="notifications-group-title">
-				Most common Notifications
+				<?php esc_html_e( 'Common Notifications', 'notification' ); ?>
 			</h2>
-			<div class="notifications-list">
-				<div class="notifications-tile">
-					<div class="inside">
-						<div class="content">
-							<h2 class="hndle"><span>Post published</span></h2>
-							<p>When post is accepted and published an email is sent to the post author.</p>
-							<div class="trigger-type"><span class="dashicons dashicons-admin-users"></span>Post author</div>
-						</div>
-						<div class="carrier-type">Email</div>
-						<input type="checkbox" name="notification_name">
-					</div>
-					<div class="notifications-tile-hover">
-						<span class="dashicons dashicons-plus"></span>
-						Add this notification
-					</div>
-				</div>
-				<div class="notifications-tile">
-					<div class="inside">
-						<div class="content">
-							<h2 class="hndle"><span>Post published</span></h2>
-							<p>When post is accepted and published an email is sent to the post author.</p>
-							<div class="trigger-type"><span class="dashicons dashicons-admin-users"></span>Post author</div>
-						</div>
-						<div class="carrier-type">Email</div>
-						<input type="checkbox" name="notification_name">
-					</div>
-					<div class="notifications-tile-hover">
-						<span class="dashicons dashicons-plus"></span>
-						Add this notification
-					</div>
-				</div>
-				<div class="notifications-tile">
-					<div class="inside">
-						<div class="content">
-							<h2 class="hndle"><span>Post published</span></h2>
-							<p>When post is accepted and published an email is sent to the post author.</p>
-							<div class="trigger-type"><span class="dashicons dashicons-admin-users"></span>Post author</div>
-						</div>
-						<div class="carrier-type">Email</div>
-						<input type="checkbox" name="notification_name">
-					</div>
-					<div class="notifications-tile-hover">
-						<span class="dashicons dashicons-plus"></span>
-						Add this notification
-					</div>
-				</div>
-				<div class="notifications-tile">
-					<div class="inside">
-						<div class="content">
-							<h2 class="hndle"><span>Post published</span></h2>
-							<p>When post is accepted and published an email is sent to the post author.</p>
-							<div class="trigger-type"><span class="dashicons dashicons-admin-users"></span>Post author</div>
-						</div>
-						<div class="carrier-type">Email</div>
-						<input type="checkbox" name="notification_name">
-					</div>
-					<div class="notifications-tile-hover">
-						<span class="dashicons dashicons-plus"></span>
-						Add this notification
-					</div>
-				</div>
-				<div class="notifications-tile">
-					<div class="inside">
-						<div class="content">
-							<h2 class="hndle"><span>Post published</span></h2>
-							<p>When post is accepted and published an email is sent to the post author.</p>
-							<div class="trigger-type"><span class="dashicons dashicons-admin-users"></span>Post author</div>
-						</div>
-						<div class="carrier-type">Email</div>
-						<input type="checkbox" name="notification_name">
-					</div>
-					<div class="notifications-tile-hover">
-						<span class="dashicons dashicons-plus"></span>
-						Add this notification
-					</div>
-				</div>
-				<div class="notifications-tile">
-					<div class="inside">
-						<div class="content">
-							<h2 class="hndle"><span>Post published</span></h2>
-							<p>When post is accepted and published an email is sent to the post author.</p>
-							<div class="trigger-type"><span class="dashicons dashicons-admin-users"></span>Post author</div>
-						</div>
-						<div class="carrier-type">Email</div>
-						<input type="checkbox" name="notification_name">
-					</div>
-					<div class="notifications-tile-hover">
-						<span class="dashicons dashicons-plus"></span>
-						Add this notification
-					</div>
-				</div>
-				<div class="notifications-tile">
-					<div class="inside">
-						<div class="content">
-							<h2 class="hndle"><span>Post published</span></h2>
-							<p>When post is accepted and published an email is sent to the post author.</p>
-							<div class="trigger-type"><span class="dashicons dashicons-admin-users"></span>Post author</div>
-						</div>
-						<div class="carrier-type">Email</div>
-						<input type="checkbox" name="notification_name">
-					</div>
-					<div class="notifications-tile-hover">
-						<span class="dashicons dashicons-plus"></span>
-						Add this notification
-					</div>
-				</div>
-				<div class="notifications-tile">
-					<div class="inside">
-						<div class="content">
-							<h2 class="hndle"><span>Post published</span></h2>
-							<p>When post is accepted and published an email is sent to the post author.</p>
-							<div class="trigger-type"><span class="dashicons dashicons-admin-users"></span>Post author</div>
-						</div>
-						<div class="carrier-type">Email</div>
-						<input type="checkbox" name="notification_name">
-					</div>
-					<div class="notifications-tile-hover">
-						<span class="dashicons dashicons-plus"></span>
-						Add this notification
-					</div>
-				</div>
-			</div>
-
-		</div>
-
-		<div class="notifications-group">
-			<h2 class="notifications-group-title">
-				Most common Notifications
-			</h2>
-			<h4 class="notifications-group-subtitle">
+			<!-- <h4 class="notifications-group-subtitle">
 				Select the email to overwrite it
-			</h4>
+			</h4> -->
 			<div class="notifications-list">
-
+				<?php for ( $i = 0; $i <= 10; $i++ ) : ?>
+				<div class="notifications-tile">
+					<div class="inside">
+						<div class="content">
+							<h2 class="hndle"><label><input type="checkbox" name="notification_name"> Post published and even more text here</label></h2>
+							<p>When post is accepted and published an email is sent to the post author.</p>
+							<div class="trigger-type"><span class="dashicons dashicons-admin-users"></span>Post author</div>
+						</div>
+						<div class="carrier-type">Email</div>
+					</div>
+					<div class="notifications-tile-hover">
+						<span class="dashicons dashicons-plus"></span>
+						Add this notification
+					</div>
+				</div>
+				<?php endfor ?>
 			</div>
+
 		</div>
 
 	</div>
