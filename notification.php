1505dd445953d9c70c82d9205df5e654118c441f
Gregoire <klesk44@gmail.com>
2019-06-24 09:37:28 +0200

    PHP 5 compliance

--- notification.php
+++ notification.php
@@ -14,7 +14,7 @@
 
 define( 'NOTIFICATION_VERSION', '6.0.4' );
 
-require_once __DIR__ . '/vendor/autoload.php';
+require_once dirname(__FILE__) . '/vendor/autoload.php';
 
 /**
  * Requirements check
