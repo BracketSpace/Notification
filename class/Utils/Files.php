9fb138ff62b0a4b911226fa39320cab356a62fd4
Jakub Mikita <jakub@bracketspace.com>
2019-07-23 09:08:23 +0200

    simplify return

--- class/Utils/Files.php
+++ class/Utils/Files.php
@@ -187,8 +187,7 @@ class Files {
 	 */
 	public function asset_mtime( $type = '', $file = '' ) {
 		$path = $this->asset_path( $type, $file );
-		$time = file_exists( $path ) ? filemtime( $path ) : time();
-		return $time;
+		return file_exists( $path ) ? filemtime( $path ) : time();
 	}
 
 	/**
