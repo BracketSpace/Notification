3eab6b275e53984e5880304b841656bee8b1037e
Jakub Mikita <jakub@bracketspace.com>
2020-02-21 08:08:12 +0100

    comment type trait

--- src/classes/Traits/Cache.php
+++ src/classes/Traits/Cache.php
@@ -13,6 +13,12 @@ namespace BracketSpace\Notification\Traits;
 trait Cache {
 
 	/**
+	 * ====================================
+	 * Post type
+	 * ====================================
+	 */
+
+	/**
 	 * Gets nice, translated post name for post type slug
 	 *
 	 * @since  [Next]
@@ -42,6 +48,44 @@ trait Cache {
 	 */
 	public function get_post_type() {
 		return $this->post_type;
+	}
+
+	/**
+	 * ====================================
+	 * Comment type
+	 * ====================================
+	 */
+
+	/**
+	 * Gets nice, translated comment type
+	 *
+	 * @since  [Next]
+	 * @return string
+	 */
+	public function get_current_comment_type_name() {
+		return self::get_comment_type_name( $this->get_comment_type() );
+	}
+
+	/**
+	 * Gets nice, translated post name for post type slug
+	 *
+	 * @since  [Next]
+	 * @param  string $comment_type Comment type slug.
+	 * @return string               Comment type name.
+	 */
+	public static function get_comment_type_name( $comment_type ) {
+		$comment_types = notification_cache( 'comment_types' );
+		return $comment_types[ $comment_type ] ?? '';
+	}
+
+	/**
+	 * Gets comment type slug
+	 *
+	 * @since  [Next]
+	 * @return string
+	 */
+	public function get_comment_type() {
+		return $this->comment_type;
 	}
 
 }
