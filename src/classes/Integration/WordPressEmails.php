1f5d8d136dff50b8e5a188b349511ac50171e4d7
Jakub Mikita <jakub@bracketspace.com>
2020-02-21 15:40:46 +0100

    integrate with woocommerce's password reset
    #3eg43c

--- src/classes/Integration/WordPressEmails.php
+++ src/classes/Integration/WordPressEmails.php
@@ -104,6 +104,7 @@ class WordPressEmails {
 		if ( 'true' !== notification_get_setting( 'integration/emails/password_change_to_admin' ) ) {
 			return;
 		}
+		add_filter( 'woocommerce_disable_password_change_notification', '__return_true' );
 		remove_action( 'after_password_reset', 'wp_password_change_notification' );
 	}
 
