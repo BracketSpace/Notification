cb0ab79abe8bb545103ab56ecde741f6a3ee9242
Viktoriia Iasynetska <viktoriia.iasynetska@gmail.com>
2020-05-28 09:50:19 +0200

    code refactoring #5cqbr2

--- src/classes/Defaults/Trigger/Post/PostApproved.php
+++ src/classes/Defaults/Trigger/Post/PostApproved.php
@@ -49,8 +49,8 @@ class PostApproved extends PostTrigger {
 
 		$this->{ $this->post_type } = $post;
 
-		$this->author          = get_userdata( $this->{ $this->post_type }->post_author );
-		$this->last_editor     = get_userdata( get_post_meta( $this->{ $this->post_type }->ID, '_edit_last', true ) );
+		$this->author         = get_userdata( $this->{ $this->post_type }->post_author );
+		$this->last_editor    = get_userdata( get_post_meta( $this->{ $this->post_type }->ID, '_edit_last', true ) );
 		$this->approving_user = get_userdata( get_current_user_id() );
 
 		$this->{ $this->post_type . '_creation_datetime' }     = strtotime( $this->{ $this->post_type }->post_date_gmt );
