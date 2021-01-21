656f4342a55f6066a919cd8a162381f20381f345
Jakub Mikita <jakub@bracketspace.com>
2021-01-21 07:40:36 +0100

    fix tests db connection

--- tests/wp-tests-config.php
+++ tests/wp-tests-config.php
@@ -37,10 +37,10 @@ define( 'WP_DEBUG', true );
  * DO NOT use a production database or one that is shared with something else.
  */
 
-define( 'DB_NAME', 'wp_phpunit_tests' );
-define( 'DB_USER', 'root' );
-define( 'DB_PASSWORD', 'root' );
-define( 'DB_HOST', 'localhost' );
+define( 'DB_NAME', getenv( 'WP_DB_NAME' ) ?: 'wp_phpunit_tests' );
+define( 'DB_USER', getenv( 'WP_DB_USER' ) ?: 'root' );
+define( 'DB_PASSWORD', getenv( 'WP_DB_PASS' ) ?: 'root' );
+define( 'DB_HOST', getenv( 'WP_DB_HOST' ) ?: 'localhost' );
 define( 'DB_CHARSET', 'utf8' );
 define( 'DB_COLLATE', '' );
 
