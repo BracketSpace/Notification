2afb046ebdaf0d42866393c2a05c55d06b8fd7b6
Jakub Mikita <jakub@bracketspace.com>
2021-01-21 07:27:57 +0100

    change unit tests engine

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -7,29 +7,18 @@
 
 define( 'NOTIFICATION_DOING_TESTS', true );
 
-$_tests_dir = getenv( 'WP_TESTS_DIR' );
-
-if ( ! $_tests_dir ) {
-	$_tests_dir = rtrim( sys_get_temp_dir(), '/\\' ) . '/wordpress-tests-lib';
-}
-
-if ( ! file_exists( $_tests_dir . '/includes/functions.php' ) ) {
-	echo "Could not find $_tests_dir/includes/functions.php, have you run bin/install-wp-tests.sh ?";
-	exit( 1 );
-}
+// Composer autoloader must be loaded before WP_PHPUNIT__DIR will be available
+require_once dirname( __DIR__ ) . '/vendor/autoload.php';
 
 // Give access to tests_add_filter() function.
-require_once $_tests_dir . '/includes/functions.php';
+require_once getenv( 'WP_PHPUNIT__DIR' ) . '/includes/functions.php';
 
 /**
  * Manually load the plugin being tested.
  */
 tests_add_filter( 'muplugins_loaded', function() {
-	require dirname( dirname( __FILE__ ) ) . '/notification.php';
+	require dirname( __DIR__ ) . '/notification.php';
 } );
 
-tests_add_filter( 'notification/load/default/recipients', '__return_false' );
-tests_add_filter( 'notification/load/default/resolvers', '__return_false' );
-
 // Start up the WP testing environment.
-require $_tests_dir . '/includes/bootstrap.php';
+require getenv( 'WP_PHPUNIT__DIR' ) . '/includes/bootstrap.php';
