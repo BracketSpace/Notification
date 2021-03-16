2f494a3fc45a0b00dc18602cdce36ea78ab04e01
Viktor Szépe <viktor@szepe.net>
2021-03-16 19:14:06 +0100

    Remove duplicated ini from load.php
    Already done in https://github.com/BracketSpace/Notification/blob/cfa33f53c16badf26a17e5dc591788fadcfe03d1/notification.php#L95-L97

--- load.php
+++ load.php
@@ -9,10 +9,3 @@
  * Load the main plugin file.
  */
 require_once __DIR__ . '/notification.php';
-
-/**
- * Initialize early.
- */
-add_action( 'init', function() {
-	Notification::init( __FILE__ )->init();
-}, 4 );
