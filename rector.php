01451f653cfd398f7a2e00c41b83591b7f2338a8
Viktor Szépe <viktor@szepe.net>
2021-05-05 11:02:56 +0000

    Fix CS

--- rector.php
+++ rector.php
@@ -5,18 +5,18 @@ use Rector\Set\ValueObject\DowngradeSetList;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
-    $parameters = $containerConfigurator->parameters();
-    $parameters->set(Option::SETS, [
-        DowngradeSetList::PHP_74,
-        DowngradeSetList::PHP_73,
-        DowngradeSetList::PHP_72,
-        DowngradeSetList::PHP_71,
-    ]);
-    $parameters->set(Option::PATHS, [
-        __DIR__ . '/src',
-        __DIR__ . '/tests',
-    ]);
-    $parameters->set(Option::SKIP, [
-        __DIR__ . '/tests/_wordpress',
-    ]);
+	$parameters = $containerConfigurator->parameters();
+	$parameters->set(Option::SETS, [
+		DowngradeSetList::PHP_74,
+		DowngradeSetList::PHP_73,
+		DowngradeSetList::PHP_72,
+		DowngradeSetList::PHP_71,
+	]);
+	$parameters->set(Option::PATHS, [
+		__DIR__ . '/src',
+		__DIR__ . '/tests',
+	]);
+	$parameters->set(Option::SKIP, [
+		__DIR__ . '/tests/_wordpress',
+	]);
 };
