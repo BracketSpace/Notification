06cfa01fc4b99ee89d208c4041e6af369910f218
Jakub Mikita <jakub@bracketspace.com>
2021-10-08 11:58:33 +0200

    move deprecated class function to compat dir

--- compat/src-deprecated/functions.php
+++ compat/src-deprecated/functions.php
@@ -15,6 +15,40 @@ use BracketSpace\Notification\Vendor\Micropackage\DocHooks\Helper as DocHooksHel
 use BracketSpace\Notification\Queries\NotificationQueries;
 
 /**
+ * Helper function.
+ * Throws a deprecation notice from deprecated class
+ *
+ * @since  6.0.0
+ * @param  string $class       Deprecated class name.
+ * @param  string $version     Version since deprecated.
+ * @param  string $replacement Replacement class.
+ * @return void
+ */
+function notification_deprecated_class( $class, $version, $replacement = null ) {
+
+	// phpcs:disable
+	if ( defined( 'WP_DEBUG' ) && WP_DEBUG ) {
+		if ( function_exists( '__' ) ) {
+			if ( ! is_null( $replacement ) ) {
+				/* translators: 1: Class name, 2: version number, 3: alternative function name */
+				trigger_error( sprintf( __('Class %1$s is <strong>deprecated</strong> since version %2$s! Use %3$s instead.'), $class, $version, $replacement ) );
+			} else {
+				/* translators: 1: Class name, 2: version number */
+				trigger_error( sprintf( __('Class %1$s is <strong>deprecated</strong> since version %2$s with no alternative available.'), $class, $version ) );
+			}
+		} else {
+			if ( ! is_null( $replacement ) ) {
+				trigger_error( sprintf( 'Class %1$s is <strong>deprecated</strong> since version %2$s! Use %3$s instead.', $class, $version, $replacement ) );
+			} else {
+				trigger_error( sprintf( 'Class %1$s is <strong>deprecated</strong> since version %2$s with no alternative available.', $class, $version ) );
+			}
+		}
+	}
+	// phpcs:enable
+
+}
+
+/**
  * Gets the plugin Runtime.
  *
  * @deprecated 7.0.0 New Notification static class should be used.
