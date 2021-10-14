075aaa5cbad6a0c652132617643bd0d6d5d710f9
Jakub Mikita <jakub@bracketspace.com>
2021-10-14 12:42:26 +0200

    add review queue upsell

---
+++ resources/templates/upsell/review-queue-switch.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Review Queue switch
+ *
+ * @package notification
+ */
+
+?>
+
+<label class="row-label" for="queue-switch">
+	<strong><?php esc_html_e( 'Catch in review queue', 'notification' ); ?></strong>
+	<a href="https://bracketspace.com/downloads/notification-review-queue/?utm_source=wp&utm_medium=notification-edit&utm_id=upsell" target="_blank" class="label-pro">PRO</a>
+</label>
+<div class="onoffswitch">
+	<input type="checkbox" class="onoffswitch-checkbox">
+	<label class="onoffswitch-label">
+		<span class="onoffswitch-inner"></span>
+		<span class="onoffswitch-switch"></span>
+	</label>
+</div>
+<div class="clear"></div>
