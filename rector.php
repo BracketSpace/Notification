a1e94655298378274402896973c76bbc120d1872
Jakub Mikita <jakub@bracketspace.com>
2021-10-21 19:30:57 +0200

    reconfigure rector

--- rector.php
+++ rector.php
@@ -5,19 +5,21 @@
  * @package notification
  */
 
+declare(strict_types=1);
+
 use Rector\Core\Configuration\Option;
 use Rector\Set\ValueObject\DowngradeSetList;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return static function ( ContainerConfigurator $container_configurator ) {
+	$container_configurator->import( DowngradeSetList::PHP_80 );
+	$container_configurator->import( DowngradeSetList::PHP_74 );
+	$container_configurator->import( DowngradeSetList::PHP_73 );
+	$container_configurator->import( DowngradeSetList::PHP_72 );
+	$container_configurator->import( DowngradeSetList::PHP_71 );
+
 	$parameters = $container_configurator->parameters();
-	$parameters->set( Option::SETS, [
-		DowngradeSetList::PHP_80,
-		DowngradeSetList::PHP_74,
-		DowngradeSetList::PHP_73,
-		DowngradeSetList::PHP_72,
-		DowngradeSetList::PHP_71,
-	] );
+
 	$parameters->set( Option::PATHS, [
 		__DIR__ . '/resources/templates',
 		__DIR__ . '/src',
