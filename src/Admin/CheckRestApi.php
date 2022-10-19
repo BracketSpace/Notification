3650326fded46e718b2d6770df81c8ce98579652
Krystian Wojciechowski <wojciechowskik1010@gmail.com>
2022-10-19 09:29:16 +0200

    chor: display admin notice in edit and create notification panels

--- src/Admin/CheckRestApi.php
+++ src/Admin/CheckRestApi.php
@@ -26,12 +26,19 @@ class CheckRestApi {
 		$message  = json_decode( wp_remote_retrieve_body( $response ), true );
 
 		$is_available = false;
+		$is_edit      = false;
 
 		if ( array_key_exists( 'data', $message ) ) {
 			$is_available = 'RestApi' === $message['data'];
 		}
 
-		if ( ! $is_available ) {
+		$current_screen = get_current_screen();
+
+		if ( $current_screen instanceof \WP_Screen ) {
+			$is_edit = 'post' === $current_screen->base && 'notification' === $current_screen->post_type;
+		}
+
+		if ( ! $is_available && $is_edit ) {
 			$class = 'notice notice-error';
 			printf( '<div class="%1$s"><p>%2$s</p></div>', esc_attr( $class ), 'The Notification plugin requires enabled REST API endpoint: notification/v1/. Please ensure your WP REST API works correctly.' );
 		}
