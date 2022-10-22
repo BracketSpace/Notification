57adaf6dacdca68943035aec1e88d837a86b69f3
Krystian Wojciechowski <wojciechowskik1010@gmail.com>
2022-10-22 13:35:31 +0200

    fix: taxonomy didnt display merge tags (#419)
    * fix: taxonomy didnt display merge tags
    
    * fix: CR changes
    
    * fix
    
    * fix: wrong taxonomy name dispalyed in triggers
    
    * fix: apply trigger_prop to taxonomy && change params
    
    * chor: add fallback to slug

--- src/Defaults/MergeTag/Taxonomy/TaxonomyName.php
+++ src/Defaults/MergeTag/Taxonomy/TaxonomyName.php
@@ -20,21 +20,22 @@ class TaxonomyName extends StringTag {
 	 * Merge tag constructor
 	 *
 	 * @since 5.2.2
-	 * @param array $params merge tag configuration params.
+	 * @param array<mixed> $params merge tag configuration params.
 	 */
 	public function __construct( $params = [] ) {
 
-		$this->set_trigger_prop( $params['taxonomy'] ?? 'taxonomy' );
+		$this->set_trigger_prop( $params['property_name'] ?? 'taxonomy' );
 
 		$args = wp_parse_args(
 			$params,
 			[
-				'slug'        => sprintf( '%s_name', $this->get_trigger_prop() ),
+				'slug'        => sprintf( '%s_name', $params['tag_name'] ?? 'taxonomy' ),
 				'name'        => __( 'Taxonomy name', 'notification' ),
 				'description' => __( 'Hello World', 'notification' ),
 				'example'     => true,
 				'group'       => __( 'Taxonomy', 'notification' ),
 				'resolver'    => function ( $trigger ) {
+
 					return $trigger->{ $this->get_trigger_prop() }->labels->singular_name ?? '';
 				},
 			]
--- src/Defaults/MergeTag/Taxonomy/TaxonomySlug.php
+++ src/Defaults/MergeTag/Taxonomy/TaxonomySlug.php
@@ -20,22 +20,22 @@ class TaxonomySlug extends StringTag {
 	 * Merge tag constructor
 	 *
 	 * @since 5.2.2
-	 * @param array $params merge tag configuration params.
+	 * @param array<mixed> $params merge tag configuration params.
 	 */
 	public function __construct( $params = [] ) {
 
-		$this->set_trigger_prop( $params['taxonomy'] ?? 'taxonomy' );
+		$this->set_trigger_prop( $params['property_name'] ?? 'taxonomy' );
 
 		$args = wp_parse_args(
 			$params,
 			[
-				'slug'        => sprintf( '%s_slug', $this->get_trigger_prop() ),
+				'slug'        => sprintf( '%s_slug', $params['tag_name'] ?? 'taxonomy' ),
 				'name'        => __( 'Taxonomy slug', 'notification' ),
 				'description' => __( 'hello-world', 'notification' ),
 				'example'     => true,
 				'group'       => __( 'Taxonomy', 'notification' ),
 				'resolver'    => function ( $trigger ) {
-					return $trigger->{ $this->get_trigger_prop() };
+					return $trigger->{ $this->get_trigger_prop() }->name ?? '';
 				},
 			]
 		);
