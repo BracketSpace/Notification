0daff33c384f1c61b0c860a9603f7f1af814f328
Krystian Wojciechowski <wojciechowskik1010@gmail.com>
2023-08-08 17:23:29 +0200

    phpcbf

--- src/Core/Processor.php
+++ src/Core/Processor.php
@@ -11,7 +11,6 @@ declare(strict_types=1);
 
 namespace BracketSpace\Notification\Core;
 
-
 use BracketSpace\Notification\Dependencies\Micropackage\Cache\Cache;
 use BracketSpace\Notification\Dependencies\Micropackage\Cache\Driver\Transient;
 use BracketSpace\Notification\Dependencies\Micropackage\Casegnostic\Casegnostic;
