04563ec6c0a99ffde975eb85a0161b1b1288d874
Jakub Mikita <jakub@bracketspace.com>
2023-08-31 13:26:40 +0200

    feat: add persistable interface

---
+++ src/Interfaces/Persistable.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Persistable interface class
+ *
+ * @package notification
+ */
+
+declare(strict_types=1);
+
+namespace BracketSpace\Notification\Interfaces;
+
+/**
+ * Persistable interface
+ */
+interface Persistable
+{
+}
