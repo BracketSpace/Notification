a679de2c236efd5dddc7fd7709b0266a0795ee96
Krzysztof Grabania <krzysztof@grabania.pl>
2024-11-21 12:16:17 +0100

    fix: escape unescaped quotes within JSON values

--- src/Repository/Converter/JsonConverter.php
+++ src/Repository/Converter/JsonConverter.php
@@ -36,6 +36,8 @@ class JsonConverter implements Convertable
 			throw $invalidException;
 		}
 
+		$data = $this->escapeQuotes($data);
+
 		$jsonData = json_decode($data, true);
 
 		if (json_last_error() !== JSON_ERROR_NONE) {
@@ -68,5 +70,41 @@ class JsonConverter implements Convertable
 		$data = $notification->to('array', ['onlyEnabledCarriers' => $onlyEnabledCarriers]);
 
 		return wp_json_encode($data, $jsonOptions);
+	}
+
+	/**
+	 * Find unescaped quotes n JSON and escapes them.
+	 *
+	 * `wp_insert_post` considers `post_content` to be escaped, so before saving
+	 * data it passes content through `wp_unslash` function. Because of that old
+	 * notifications might have unescaped quotes in content which will cause
+	 * JSON decoding errors.
+	 *
+	 * @see https://core.trac.wordpress.org/ticket/54601
+	 *
+	 * @param   string $originalData
+	 * @return  string
+	 */
+	private function escapeQuotes(string $originalData): string
+	{
+		$data = preg_replace_callback(
+			'/
+				".+?":       # key
+				\s+          # white characters after key
+				"(           # string value start quote
+					.+?      # any value content
+					[^\\\\]" # quote within value which is not escaped
+					.+?      # any value content
+				)"           # string value end quote
+				,?           # there might be comma after value
+				\n           # formatted JSON will always have new line after value
+			/x',
+			static function ($matches) {
+				return str_replace($matches[1], str_replace('"', '\\"', $matches[1]), $matches[0]);
+			},
+			$originalData
+		);
+
+		return is_string($data) ? $data : $originalData;
 	}
 }
